d9fb2e1cc117d5f106358b05a1910cb39d4de1e6
Dani Darko <119711983+dkvsl@users.noreply.github.com>
2024-05-22 04:10:56 +0200

    Update start_tensorboard.sh

--- scripts/start_tensorboard.sh
+++ scripts/start_tensorboard.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 export PYTHONUNBUFFERED=1
 echo "Starting Tensorboard"
-cd /workspace
 
 nohup tensorboard --logdir=/workspace/logs --port=6066 --host=0.0.0.0 > /workspace/logs/tensorboard.log 2>&1 &
 echo "Tensorboard Started"
