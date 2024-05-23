3b65307208132bd4923ac31b63c06f5ff131b29f
Dani Darko <119711983+dkvsl@users.noreply.github.com>
2024-05-23 04:52:51 +0200

    Update a1111_install.sh

--- scripts/a1111_install.sh
+++ scripts/a1111_install.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# Clone the git repo of the Stable Diffusion Web UI by Automatic1111
-# and set version
+# Clone Stable Diffusion WebUI
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /stable-diffusion
 cd /stable-diffusion
 # git checkout tags/${WEBUI_VERSION}
 
-# Create and activate venv
+# Create and activate environment
 python3 -m venv --system-site-packages /venv
 source /venv/bin/activate
 
@@ -25,11 +24,10 @@ git clone https://github.com/Mikubill/sd-webui-controlnet.git extensions/sd-webu
 git clone --depth=1 https://github.com/zanllp/sd-webui-infinite-image-browsing.git extensions/infinite-image-browsing
 git clone --depth=1 https://github.com/Uminosachi/sd-webui-inpaint-anything.git extensions/inpaint-anything
 
-# Install dependencies for Deforum, ControlNet, ReActor, Infinite Image Browsing,
-# After Detailer, and CivitAI Browser+ extensions
-#pip3 install basicsr
+# Install dependencies for ControlNet, Infinite Image Browsing
 cd /stable-diffusion/extensions/sd-webui-controlnet
 pip3 install -r requirements.txt
+pip3 install ultralytics
 
 cd /stable-diffusion/extensions/infinite-image-browsing
 pip3 install -r requirements.txt
